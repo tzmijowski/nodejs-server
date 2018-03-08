82fd386edd81a74e59e96f6176a0f6f6b6a9cb21
tzm <zmijowski.tomasz@gmail.com>
2018-03-08 16:28:10 +0100

    update2
    update2

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ var express = require('express')
 var app = express()
 var path    = require("path");
 var port = 8080;
-
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
-
 app.get('/status', function(req, res){
-    res.status(200).send('OK');
+    res.status(404).send('Not Found');
 });
-
 app.listen(port, function(){
     console.log("App is listetning at port:", port);
 });
